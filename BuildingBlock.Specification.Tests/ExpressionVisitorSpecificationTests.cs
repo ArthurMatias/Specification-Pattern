d79755b0b49477fca7af2a7bf9e8216f0bebaa3b
Nico Sap <nico@sapico.me>
2020-06-07 11:36:28 +0200

    Add project files.

---
+++ BuildingBlock.Specification.Tests/ExpressionVisitorSpecificationTests.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using BuildingBlock.Specification.Tests.Models;
+using BuildingBlock.Specification.Tests.Specifications;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BuildingBlock.Specification.Extensions;
+using System.Linq.Expressions;
+using BuildingBlock.Specification.Visitors;
+using System.Collections.Generic;
+using System.Linq;
+using BuildingBlock.Specification;
+
+namespace BuildingBlock.Specification.Tests
+{
+    //https://fabiomarreco.github.io/blog/2018/specificationpattern-with-entityframework/
+    [TestClass]
+    public class EFExpressionVisitorSpecificationTests
+    {
+
+
+        [TestMethod]
+        public void SimpleEFImplementation()
+        {
+            //assign
+            var repo = new ProductRepository();
+
+            var spec = new PriceGreaterThen(10);
+
+            //act
+            var result = repo.GetProducts(spec).ToList();
+
+            Assert.IsTrue(result.All(p => spec.IsSatisfiedBy(p)), $"Not all products were matched category or price by the specification");
+        }
+
+
+        [TestMethod]
+        public void TestAndImplementation()
+        {
+            //assign
+            var repo = new ProductRepository();
+
+            var spec = new PriceGreaterThen(10)
+                .And(new PriceLesserThen(20));
+
+            //act
+            var result = repo.GetProducts(spec).ToList();
+
+            Assert.IsTrue(result.All(p => spec.IsSatisfiedBy(p)), $"Not all products were matched category or price by the specification");
+        }
+
+        [TestMethod]
+        public void TestOrImplementation()
+        {
+            //assign
+            var repo = new ProductRepository();
+
+            var spec = new PriceGreaterThen(10)
+                .Or(new PriceLesserThen(20));
+
+            //act
+            var result = repo.GetProducts(spec).ToList();
+
+            Assert.IsTrue(result.All(p => spec.IsSatisfiedBy(p)), $"Not all products were matched category or price by the specification");
+        }
+
+
+        [TestMethod]
+        public void TestNotImplementation()
+        {
+            //assign
+            var repo = new ProductRepository();
+
+            var spec = new PriceGreaterThen(10).Not();
+
+            //act
+            var result = repo.GetProducts(spec).ToList();
+
+            Assert.IsTrue(result.All(p => spec.IsSatisfiedBy(p)), $"Not all products were matched category or price by the specification");
+        }
+
+        [TestMethod]
+        public void TestEFImplementation()
+        {
+            //assign
+            var repo = new ProductRepository();
+
+            var spec = new PriceGreaterThen(10)
+                .And(new PriceLesserThen(20))
+            .Or(new ProductOfCategory("Electronics"));
+
+            //act
+            var result = repo.GetProducts(spec).ToList();
+
+            Assert.IsTrue(result.All(p => spec.IsSatisfiedBy(p)), $"Not all products were matched category or price by the specification");
+            
+
+        }
+
+        [TestMethod]
+        public void TestDeepEFImplementation()
+        {
+            //assign
+            var repo = new ProductRepository();
+
+            var spec = new ProductOfTag("Test-1");
+       
+
+            //act
+            var result = repo.GetProducts(spec).ToList();
+
+            Assert.IsTrue(result.All(p => spec.IsSatisfiedBy(p)), $"Not all products were matched category or price by the specification");
+        }
+
+        [TestMethod]
+        public void TestDeepOrEFImplementation()
+        {
+            //assign
+            var repo = new ProductRepository();
+
+            var spec = new ProductOfTag("Test-1")
+             .Or(new ProductOfTag("Test-2"));
+
+            //act
+            var result = repo.GetProducts(spec).ToList();
+
+            Assert.IsTrue(result.All(p => spec.IsSatisfiedBy(p)), $"Not all products were matched category or price by the specification");
+        }
+
+
+    }
+
+    public class ProductRepository
+    {
+        public IEnumerable<Product> GetProducts(ISpecification<Product, IProductSpecificationVisitor> spec)
+        {
+            var visitor = new ExpressionQueryProductSpecVisitor();
+            spec.Accept(visitor);
+            var expression = visitor.Expr;
+
+            
+            foreach (var efProduct in Repo.AsQueryable().Where(expression).ToList())
+            {
+                var product = ConvertPersistenceToDomain(efProduct);
+                yield return product;
+
+            }
+        }
+
+        private Product ConvertPersistenceToDomain(EFProduct entity)
+        {
+            return new Product(entity.Price, entity.Category, entity.Tags.Select(dl => dl.Name).ToList());
+        }
+
+
+        public static List<EFProduct> Repo { get; set; }
+
+        static ProductRepository()
+        {
+            Repo = new List<EFProduct>();
+            Repo.Add(new EFProduct(Guid.NewGuid(), 5, "Electronics", new List<string>() { "Test-1","Test-2","Test-3"}));
+            Repo.Add(new EFProduct(Guid.NewGuid(), 10, "Electronics", new List<string>() { "Test-1", "Test-2", "Test-3" }));
+            Repo.Add(new EFProduct(Guid.NewGuid(), 15, "Electronics", new List<string>() { "Test-1","Test-2","Test-3"}));
+            Repo.Add(new EFProduct(Guid.NewGuid(), 20, "Electronics", new List<string>() { "Test-1","Test-2","Test-3"}));
+            Repo.Add(new EFProduct(Guid.NewGuid(), 10, "Games", new List<string>() { "Test-1","Test-2","Test-3"}));
+            Repo.Add(new EFProduct(Guid.NewGuid(), 15, "Games", new List<string>() { "Test-1","Test-2","Test-3"}));
+            Repo.Add(new EFProduct(Guid.NewGuid(), 20, "Games", new List<string>() { "Test-1","Test-2","Test-3"}));
+        }
+    }
+
+
+
+
+
+}
---
+++ BuildingBlock.Specification/ISpecification.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlock.Specification
+{
+    //Generic interfaces definitions
+    //http://share.linqpad.net/rtex3r.linq
+    public interface ISpecification<in T, in TVisitor> where TVisitor : ISpecificationVisitor<TVisitor, T>
+    {
+        bool IsSatisfiedBy(T item);
+        void Accept(TVisitor visitor);
+    }
+
+    public interface ISpecificationVisitor<TVisitor, T> where TVisitor : ISpecificationVisitor<TVisitor, T>
+    {
+        void Visit(AndSpecification<T, TVisitor> spec);
+        void Visit(OrSpecification<T, TVisitor> spec);
+        void Visit(NotSpecification<T, TVisitor> spec);
+    }
+
+    //Specific implementation
+  /*  public interface IProductSpecificationVisitor : ISpecificationVisitor<IProductSpecificationVisitor, Product>
+    {
+        void Visit(ProductMatchesCategory spec);
+        void Visit(ProductPriceInRange spec);
+    }
+
+    public class ProductMatchesCategory : ISpecification<Product, IProductSpecificationVisitor>
+    {
+        public readonly string Category;
+
+        public ProductMatchesCategory(string category)
+        {
+            this.Category = category;
+        }
+
+        public bool IsSatisfiedBy(Product item) => item.Category == Category;
+
+        public void Accept(IProductSpecificationVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }*/
+}
